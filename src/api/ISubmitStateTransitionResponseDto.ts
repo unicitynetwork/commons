d1efa07a4c36b732ea964113564ebbc7bba54df6
Mart Mägi <mart.magi@guardtime.com>
2025-04-03 21:29:29 +0300

    Remove aggregator specific interfaces

--- src/api/ISubmitStateTransitionResponseDto.ts
+++
@@ -1,7 +0,0 @@
-import { IInclusionProofDto } from './InclusionProof.js';
-import { SubmitStateTransitionStatus } from './SubmitStateTransitionStatus.js';
-
-export interface ISubmitStateTransitionResponseDto {
-  readonly inclusionProof: IInclusionProofDto;
-  readonly status: SubmitStateTransitionStatus;
-}
--- src/api/SubmitStateTransitionStatus.ts
+++
@@ -1,3 +0,0 @@
-export enum SubmitStateTransitionStatus {
-  SUCCESS = 'SUCCESS',
-}
