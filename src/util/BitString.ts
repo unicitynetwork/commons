1ede71a0c261550da0b81c3d0645cb5892502bdc
Martti Marran <martti.marran@hotmail.com>
2025-06-27 13:19:37 +0300

    Updated toBitString method jsdoc

--- src/util/BitString.ts
+++ src/util/BitString.ts
@@ -1,18 +1,33 @@
-import { BigintConverter } from './BigintConverter.js';
 import { HexConverter } from './HexConverter.js';
 
 export class BitString {
+  /**
+   * Represents a bit string as a bigint.
+   * The first bit is always set to 1 to not lose leading bits when converting to bigint.
+   */
   private readonly value: bigint;
 
+  /**
+   * Creates a BitString from a Uint8Array.
+   * @param {Uint8Array} data - The input data to convert into a BitString.
+   */
   public constructor(data: Uint8Array) {
     this.value = BigInt(`0x01${HexConverter.encode(data)}`);
   }
 
+  /**
+   * Converts bit string to bigint
+   * @returns {bigint} The bigint representation of the bit string
+   */
   public toBigInt(): bigint {
     return this.value;
   }
 
+  /**
+   * Converts bit string to string
+   * @returns {string} The string representation of the bit string
+   */
   public toString(): string {
-    return `BitString[${HexConverter.encode(BigintConverter.encode(this.value))}]`;
+    return this.value.toString(2).slice(1);
   }
 }
